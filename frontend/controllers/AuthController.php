d80e1e1dc7b683eddcf766d05b067d6ca3d4ebc6
sydorenkovd <sydorenkovd@gmail.com>
2016-05-15 09:10:27 +0300

    auth

---
+++ frontend/controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Виктор Сидоренко
+ * Date: 15.05.2016
+ * Time: 9:08
+ */
+
+namespace frontend\controllers;
+use Yii;
+use yii\web\Controller;
+
+class AuthController extends Controller
+{
+    public function actionInitializeAuthorizations()
+    {
+        $auth = Yii::$app->authManager;
+
+        // Reset all
+        $auth->removeAll();
+
+        // add "createReservation" permission
+        $permCreateReservation = $auth->createPermission('createReservation');
+        $permCreateReservation->description = 'Create a reservation';
+        $auth->add($permCreateReservation);
+
+        // add "updatePost" permission
+        $permUpdateReservation = $auth->createPermission('updateReservation');
+        $permUpdateReservation->description = 'Update reservation';
+        $auth->add($permUpdateReservation);
+
+        // add "operator" role and give this role the "createReservation" permission
+        $roleOperator = $auth->createRole('operator');
+        $auth->add($roleOperator);
+        $auth->addChild($roleOperator, $permCreateReservation);
+
+        // add "admin" role and give this role the "reservation" permission
+        // as well as the permissions of the "operator" role
+        $roleAdmin = $auth->createRole('admin');
+        $auth->add($roleAdmin);
+        $auth->addChild($roleAdmin, $permUpdateReservation);
+        $auth->addChild($roleAdmin, $roleOperator);
+
+        // Assign roles to users. 1 and 2 are IDs returned by IdentityInterface::getId()
+        // usually implemented in your User model.
+        $auth->assign($roleOperator, 2);
+        $auth->assign($roleAdmin, 1);
+    }
+}
