d8277d1626aa25194eb9d95178a6455f962f1eb4
sydorenkovd <sydorenkovd@gmail.com>
2016-05-17 08:13:50 +0300

    gii for reservations

---
+++ frontend/views/reservation/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Reservation */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="reservation-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'room_id')->textInput() ?>
+
+    <?= $form->field($model, 'customer_id')->textInput() ?>
+
+    <?= $form->field($model, 'price_per_day')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'date_from')->textInput() ?>
+
+    <?= $form->field($model, 'date_to')->textInput() ?>
+
+    <?= $form->field($model, 'reservation_date')->textInput() ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
