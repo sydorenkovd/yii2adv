4a91c842269a8a93c61c70593c1e47131d65be7f
sydorenkovd <sydorenkovd@gmail.com>
2016-05-21 19:44:53 +0300

    exect news

--- frontend/controllers/YandexController.php
+++ frontend/controllers/YandexController.php
@@ -9,7 +9,6 @@ class YandexController extends \yii\web\Controller
 {
     public function actionYandex()
     {
-
 // создаем экземпляр класса
         $client = new Client();
 // отправляем запрос к странице Яндекса
@@ -18,5 +17,20 @@ class YandexController extends \yii\web\Controller
         $document = $res->getBody();
 // вывод страницы Яндекса в представление
         return $this->render('yandex', ['document' => $document]);
+    }
+    public function actionYanews() {
+
+        // создаем экземпляр класса
+        $client = new Client();
+        // отправляем запрос к странице Яндекса
+        $res = $client->request('GET', 'http://www.yandex.ru');
+        // получаем данные между открывающим и закрывающим тегами body
+        $body = $res->getBody();
+        // подключаем phpQuery
+        $document = \phpQuery::newDocumentHTML($body);
+        //Смотрим html страницы Яндекса, определяем внешний класс списка и считываем его командой find
+        $news = $document->find(".b-news-list");
+        // вывод списка новостей Яндекса с главной страницы в представление
+        return $this->render('yanews', ['news' => $news]);
     }
 }
