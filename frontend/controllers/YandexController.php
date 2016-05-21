e021e577a5273ee8c3f1560b8b1740014425abf8
sydorenkovd <sydorenkovd@gmail.com>
2016-05-21 19:56:14 +0300

    parsers yandex ua

--- frontend/controllers/YandexController.php
+++ frontend/controllers/YandexController.php
@@ -18,7 +18,9 @@ class YandexController extends \yii\web\Controller
 // вывод страницы Яндекса в представление
         return $this->render('yandex', ['document' => $document]);
     }
-    public function actionYanews() {
+
+    public function actionYanews()
+    {
 
         // создаем экземпляр класса
         $client = new Client();
@@ -32,5 +34,35 @@ class YandexController extends \yii\web\Controller
         $news = $document->find(".b-news-list");
         // вывод списка новостей Яндекса с главной страницы в представление
         return $this->render('yanews', ['news' => $news]);
+    }
+
+    public function actionYandexNewsList()
+    {
+
+        // создаем экземпляр класса
+        $client = new Client();
+        // отправляем запрос к странице Яндекса
+        $res = $client->request('GET', 'http://www.yandex.ua');
+        // получаем данные между открывающим и закрывающим тегами body
+        $body = $res->getBody();
+        // подключаем phpQuery
+        $document = \phpQuery::newDocumentHTML($body);
+        // получаем список новостей
+        $news = $document->find("ol.b-news-list");
+        // выполняем проход циклом по списку
+        foreach ($news as $elem) {
+            //pq аналог $ в jQuery
+            $pq = pq($elem);
+            // удалим первую новость в списке
+            $pq->find('li.list__item:first')->remove();
+            // выполним поиск в скиске ссылок
+            $tags = $pq->find('li.list__item a');
+            // добавим ковычки в начало и в конец предложения
+            $tags->append('" ')->prepend(' "'); //
+            // добавим свой класс к последней новости списка
+            $pq->find('li.list__item:last')->addClass('my_last_class');
+        }
+        // вывод списка новостей яндекса с главной страницы в представление
+        return $this->render('yandex-news-list', ['news' => $news]);
     }
 }
